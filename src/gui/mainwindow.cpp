3f7f247d462d6cd77821e4306ac18a6baf80adb1
Charles <charles.donnat@gmail.com>
2011-07-21 23:27:15 +0200

    init position

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -2,8 +2,11 @@
 
 #include "serieswidget.h"
 
+#include <QApplication>
+#include <QDesktopWidget>
 #include <QAction>
 #include <QCloseEvent>
+#include <QPointF>
 #include <QToolBar>
 
 //----------------------------------------------------------------------------------------------
@@ -12,9 +15,9 @@ MainWindow::MainWindow():QMainWindow(),_settings("DocDoc", "QSeriesHunter")
     _seriesWidget = new SeriesWidget(this);
 
     _toolBar = new QToolBar(this);
-    _add     = new QAction(QIcon(":images/add"), tr("Add"), _toolBar);
+    _add     = new QAction(QIcon(":images/add")       , tr("Add")   , _toolBar);
     _remove  = new QAction(QIcon(":images/remove.png"), tr("Remove"), _toolBar);
-    _edit    = new QAction(QIcon(":images/edit.png"), tr("Edit"), _toolBar);
+    _edit    = new QAction(QIcon(":images/edit.png")  , tr("Edit")  , _toolBar);
     _update  = new QAction(QIcon(":images/update.png"), tr("Update"), _toolBar);
 
     _toolBar->addAction (_add);
@@ -22,9 +25,9 @@ MainWindow::MainWindow():QMainWindow(),_settings("DocDoc", "QSeriesHunter")
     _toolBar->addAction (_edit);
     _toolBar->addAction (_update);
 
-    connect(_add, SIGNAL(triggered()), _seriesWidget, SLOT(add()));
+    connect(_add   , SIGNAL(triggered()), _seriesWidget, SLOT(add()));
     connect(_update, SIGNAL(triggered()), _seriesWidget, SLOT(update()));
-    connect(_edit, SIGNAL(triggered()), _seriesWidget, SLOT(edit()));
+    connect(_edit  , SIGNAL(triggered()), _seriesWidget, SLOT(edit()));
     connect(_remove, SIGNAL(triggered()), _seriesWidget, SLOT(remove()));
 
     this->addToolBar (_toolBar);
@@ -37,8 +40,15 @@ MainWindow::MainWindow():QMainWindow(),_settings("DocDoc", "QSeriesHunter")
 //----------------------------------------------------------------------------------------------
 void MainWindow::loadSettings()
 {
-    move (_settings.value ("position").toPoint ());
-    setGeometry (_settings.value ("size").toRect ());
+	this->adjustSize();
+	QPointF widgetPosition = 
+		QApplication::desktop()->screen()->rect().center() - this->rect().center();
+	
+	if (_settings.contains("position")) {
+		widgetPosition = _settings.value ("position").toPoint ();
+		setGeometry (_settings.value ("size").toRect ());		
+	}
+	move (widgetPosition.x(), widgetPosition.y());	
 }
 
 //----------------------------------------------------------------------------------------------
